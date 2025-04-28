5e17c0addd4508900d7fab8fbcedf7f483540291
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2025-04-28 16:54:17 +0200

    UI for non auth user

---
+++ lib/components/loginRequest.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../login/login.dart';
+
+
+class requestLogin extends StatefulWidget {
+  const requestLogin({super.key});
+
+  @override
+  State<requestLogin> createState() => _requestLoginState();
+}
+
+class _requestLoginState extends State<requestLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.lock_outline,
+                size: 100,
+                color: Colors.blueAccent,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "Per ottenere badge esclusivi e sfidare i tuoi amici, accedi al tuo account.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => LoginForm()),
+                  );
+                },
+                icon: Icon(Icons.login, color: Colors.white),
+                label: const Text("Fai il login", style: TextStyle(fontSize: 16)),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white, backgroundColor: Colors.blueAccent, padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "Unisciti alla community, conquista obiettivi e supera i tuoi amici!",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black54,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
