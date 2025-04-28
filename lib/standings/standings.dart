eafe5f552011b9c6bfed03c63a8c57fab6479384
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2025-04-28 16:54:48 +0200

    screen avaiable only for auth users

--- lib/standings/standings.dart
+++ lib/standings/standings.dart
@@ -1,3 +1,4 @@
+import 'package:delta_explorer/components/loginRequest.dart';
 import 'package:delta_explorer/constants/point.dart';
 import 'package:delta_explorer/login/login.dart';
 import 'package:delta_explorer/standings/standingController.dart';
@@ -46,61 +47,7 @@ class _StandingsState extends State<Standings> {
         ),
       );
     } else {
-      return Scaffold(
-
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.lock_outline,
-                  size: 100,
-                  color: Colors.blueAccent,
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  "Per ottenere badge esclusivi e sfidare i tuoi amici, accedi al tuo account.",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black87,
-                  ),
-                ),
-                const SizedBox(height: 30),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => LoginForm()),
-                    );
-                  },
-                  icon: const Icon(Icons.login, color: Colors.white),
-                  label: const Text("Fai il login", style: TextStyle(fontSize: 16)),
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white, backgroundColor: Colors.blueAccent, padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  "Unisciti alla community, conquista obiettivi e supera i tuoi amici!",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontStyle: FontStyle.italic,
-                    color: Colors.black54,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      );
+      return requestLogin();
     }
   }
 
