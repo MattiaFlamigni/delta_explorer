0ecd7da52d750c30a095ddedc0f5239118c16c59
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2025-05-02 10:43:50 +0200

    performed user UI experience

--- lib/gallery/gallery.dart
+++ lib/gallery/gallery.dart
@@ -1,5 +1,7 @@
 import 'package:delta_explorer/gallery/galleryController.dart';
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
+
 
 class GalleryScreen extends StatefulWidget {
   const GalleryScreen({super.key});
@@ -19,49 +21,169 @@ class _GalleryScreenState extends State<GalleryScreen> {
     });
   }
 
+  // Funzione per mostrare l'immagine a schermo intero
+  void _showFullScreenImage(BuildContext context, String imageUrl, int index, List<Map<String, dynamic>> allImages) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => FullScreenGallery(
+          initialIndex: index,
+          imageUrls: allImages.where((img) => img["image_path"]?.isNotEmpty == true).map((img) => "https://cvperzyahqhkdcjjtqvm.supabase.co/storage/v1/object/public/${img["image_path"]}").toList(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final validImages = controller.getSpottedList().where((img) => img["image_path"]?.isNotEmpty == true).toList();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Galleria')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0), // Aggiunge un po' di spazio intorno alla griglia
+        child: validImages.isEmpty
+            ? const Center(child: Text("Nessuna immagine disponibile."))
+            : GridView.builder(
+          itemCount: validImages.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 8, // Aumenta la spaziatura tra le immagini
+            mainAxisSpacing: 8,
+          ),
+          itemBuilder: (context, index) {
+            var imagePath = validImages[index]["image_path"];
+            final imageUrl = "https://cvperzyahqhkdcjjtqvm.supabase.co/storage/v1/object/public/$imagePath";
+
+            return GestureDetector(
+              onTap: () {
+                _showFullScreenImage(context, imageUrl, index, controller.getSpottedList());
+              },
+              child: Stack(
+                children: [
+                  Hero(
+                    tag: 'image_$index',
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage, // Usa kTransparentImage o un Uint8List vuoto
+                        image: imageUrl,
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                        height: double.infinity,
+                        imageErrorBuilder: (context, error, stackTrace) {
+                          return const Center(
+                            child: Icon(Icons.broken_image, size: 50, color: Colors.grey),
+                          );
+                        },
+                        placeholderErrorBuilder: (context, error, stackTrace) {
+                          return const Center(
+                            child: Icon(Icons.image_not_supported, size: 50, color: Colors.grey),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  // Opzionale: Aggiungere un leggero overlay o un'icona al tocco
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+// Widget per la visualizzazione a schermo intero con possibilità di scorrere
+class FullScreenGallery extends StatefulWidget {
+  final int initialIndex;
+  final List<String> imageUrls;
+
+  const FullScreenGallery({super.key, required this.initialIndex, required this.imageUrls});
+
+  @override
+  State<FullScreenGallery> createState() => _FullScreenGalleryState();
+}
+
+class _FullScreenGalleryState extends State<FullScreenGallery> {
+  late PageController _pageController;
+  int _currentPageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentPageIndex = widget.initialIndex;
+    _pageController = PageController(initialPage: widget.initialIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Galleria')),
-      body:
-          controller.getSpottedList().isEmpty
-              ? const Center(child: Text("No Images"))
-              : GridView.builder(
-                itemCount: controller.getSpottedList().length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, // due immagini per riga
-                  crossAxisSpacing: 4,
-                  mainAxisSpacing: 4,
-                ),
-                itemBuilder: (context, index) {
-                  var imagePath =
-                      controller.getSpottedList()[index]["image_path"];
-
-                  if (imagePath != null && imagePath.toString().isNotEmpty) {
-                    return Image.network(
-                      "https://cvperzyahqhkdcjjtqvm.supabase.co/storage/v1/object/public/$imagePath",
-                      fit: BoxFit.cover,
+      appBar: AppBar(
+        backgroundColor: Colors.black.withOpacity(0.7),
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          PageView.builder(
+            controller: _pageController,
+            itemCount: widget.imageUrls.length,
+            onPageChanged: (index) {
+              setState(() {
+                _currentPageIndex = index;
+              });
+            },
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.pop(context); // Chiude la visualizzazione a schermo intero al tap
+                },
+                child: Hero(
+                  tag: 'image_$_currentPageIndex', // Usa l'indice corrente per il tag Hero
+                  child: InteractiveViewer( // Permette lo zoom e il pan
+                    panEnabled: false, // Disabilita il pan orizzontale se usi PageView
+                    boundaryMargin: const EdgeInsets.all(double.infinity),
+                    minScale: 0.5,
+                    maxScale: 5.0,
+                    child: Image.network(
+                      widget.imageUrls[index],
+                      fit: BoxFit.contain,
                       loadingBuilder: (context, child, loadingProgress) {
-                        if (loadingProgress == null) return child;
-                        return const Center(child: CircularProgressIndicator());
+                        return loadingProgress == null
+                            ? child
+                            : const Center(child: CircularProgressIndicator(color: Colors.white));
                       },
                       errorBuilder: (context, error, stackTrace) {
-                        return const Icon(
-                          Icons.broken_image,
-                          size: 50,
-                          color: Colors.grey,
-                        );
+                        return const Center(child: Icon(Icons.broken_image, size: 80, color: Colors.white));
                       },
-                    );
-                  } else {
-                    return const Icon(
-                      Icons.image_not_supported,
-                      size: 50,
-                      color: Colors.grey,
-                    );
-                  }
-                },
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+          if (widget.imageUrls.length > 1)
+            Positioned(
+              bottom: 20,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Text(
+                  '${_currentPageIndex + 1}/${widget.imageUrls.length}',
+                  style: const TextStyle(color: Colors.white, fontSize: 16),
+                ),
               ),
+            ),
+        ],
+      ),
     );
   }
 }
