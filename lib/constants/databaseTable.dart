28188817779ac282c028d9b29d83dbe025e51395
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2025-05-03 17:22:26 +0200

    created constant for database table name

---
+++ lib/constants/databaseTable.dart
@@ -0,0 +1,16 @@
+class DatabaseTable{
+
+  static const String categorie_animali = "categorie_animali";
+  static const String coordinate = "coordinate";
+  static const String curiosity = "curiosity";
+  static const String friends = "friends";
+  static const String percorsi = "percorsi";
+  static const String poi = "poi";
+  static const String points = "points";
+  static const String reports = "reports";
+  static const String reports_category = "reports_category";
+  static const String spotted = "spotted";
+  static const String tripImages = "tripImages";
+  static const String users = "users";
+
+}
